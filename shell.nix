c92ad0a57dc9bc3a1c4e3788a4c7a8d18de37751
Alex Walpole <alexcwalpole@gmail.com>
2025-10-11 23:23:05 -0400

    messing around with a shell nix config

---
+++ shell.nix
@@ -0,0 +1,18 @@
+
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    # project specific
+    nodejs_20
+    nodejs_18
+
+    # me
+    neovim
+    fish
+  ];
+
+}
