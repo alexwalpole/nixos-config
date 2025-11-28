609d5bf7c58b0ebc6964c3d2525743947e839ab0
Alex Walpole <alexcwalpole@gmail.com>
2025-11-28 16:46:27 -0500

    fish config tweaks

--- home/fish.nix
+++ home/fish.nix
@@ -3,6 +3,21 @@
   programs.fish = {
 
     enable = true;
+
+
+    functions = {
+      fish_right_prompt = ''
+           set -l node_version (node --version)
+           set -l statuscolor (set_color -o $fish_color_keyword)
+           set -l node_version "$statuscolor$node_version"
+           echo -n -s $node_version 
+        '';
+    };
+      
+      
+
+
+
     shellAbbrs = {
       gcm  = "git commit -m";
       gcob = "git checkout -b";
@@ -33,5 +48,43 @@
       lg = "lazygit";
 
     };
+    shellInit = ''
+set -l foreground cdcecf
+set -l selection 2b3b51
+set -l comment 738091
+set -l red c94f6d
+set -l orange f4a261
+set -l yellow dbc074
+set -l green 81b29a
+set -l purple 9d79d6
+set -l cyan 63cdcf
+set -l pink d67ad2
+
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
+
+set fish_greeting wassssupp
+set -gx EDITOR nvim
+    '';
+
   };
 }
