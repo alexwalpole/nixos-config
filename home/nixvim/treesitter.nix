9216239f4717b2855bad56e8888a7f5d63894016
Alex <alexcwalpole@gmail.com>
2025-12-20 18:53:33 -0500

    fix treesitter can't create parser dirs?
    i think im losing some of my settings with this change but idk what they
    were doing anyway

--- home/nixvim/treesitter.nix
+++ home/nixvim/treesitter.nix
@@ -1,32 +1,7 @@
 {
   programs.nixvim.plugins.treesitter = {
     enable = true;
-
-    settings = {
-      auto_install = true;
-      sync_install = false;
-      highlight = {
-        enable = true;
-        additonal_vim_regex_highlighting = false;
-        ensure_installed = [
-          "javascript"
-          "typescript"
-          "c"
-          "lua"
-          "vim"
-          "vimdoc"
-          "query"
-          "markdown"
-          "markdown_inline"
-          "toml"
-          "tsx"
-          "json"
-          "html"
-          "css"
-          "bash"
-          "nix"
-        ];
-      };
-    };
+    highlight.enable = true;
+    indent.enable = true;
   };
 }
