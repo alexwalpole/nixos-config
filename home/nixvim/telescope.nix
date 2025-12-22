0582b44606749dcd10a8e87fb036a11d0ba3bb50
Alex <alexcwalpole@gmail.com>
2025-12-21 20:05:09 -0500

    removing comments

--- home/nixvim/telescope.nix
+++ home/nixvim/telescope.nix
@@ -3,17 +3,12 @@
 {
   programs.nixvim = {
   keymaps = [
-    # vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Telescope find files' })
     {
       mode = "n";
       key = "<leader>pf";
-      # The raw action calls the require('telescope.builtin').find_files() function
       action.__raw = "function() require('telescope.builtin').find_files() end";
       options.desc = "Telescope find files";
     }
-
-
-    # vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'git files' })
     {
       mode = "n";
       key = "<C-p>";
@@ -21,15 +16,9 @@
       action.__raw = "function() require('telescope.builtin').git_files() end";
       options.desc = "Telescope git files";
     }
-
-
-    # vim.keymap.set('n', '<leader>ps', function()
-    # 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-    # end)
     {
       mode = "n";
       key = "<leader>ps";
-      # This needs the full Lua function since it uses vim.fn.input
       action.__raw = ''
         function() 
           require('telescope.builtin').grep_string({ search = vim.fn.input("Grep > ") }) 
@@ -37,13 +26,9 @@
       '';
       options.desc = "Telescope Grep string";
     }
-
-
-    # vim.keymap.set('n', '<leader>fr', builtin.lsp_references)
     {
       mode = "n";
       key = "<leader>fr";
-      # The raw action calls the require('telescope.builtin').lsp_references() function
       action.__raw = "function() require('telescope.builtin').lsp_references() end";
       options.desc = "Telescope LSP references";
     }
