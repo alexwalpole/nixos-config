125fb9649a73b3d6cf3f488c06646346ac41d966
Alex <alexcwalpole@gmail.com>
2025-12-21 20:06:15 -0500

    ghostty uses fish from home-manager

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -31,7 +31,7 @@ in
         window-height = 40;
         window-width = 140;
         font-size = 24;
-        command = "${pkgs.fish}/bin/fish --login --interactive";
+        command = "${config.home.profileDirectory}/bin/fish --login --interactive";
 # shell
 
 # # theme = Catppuccin Mocha
