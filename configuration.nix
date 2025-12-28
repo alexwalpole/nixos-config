1fdcf06bd4d05fdd847c63aecd1b64c7d5072222
Alex Walpole <alexcwalpole@gmail.com>
2025-12-28 15:02:11 -0500

    chore(nixos): config changes post 25.11

--- configuration.nix
+++ configuration.nix
@@ -64,18 +64,10 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -179,7 +171,7 @@
     wezterm
     ghostty
 
-    (llama-cpp.override { cudaSupport = true; })
+    # (llama-cpp.override { cudaSupport = true; })
   ];
 
   programs.steam = {
