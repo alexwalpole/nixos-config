e41753334f7c247e6856a47e419ed9e29cf8ca52
Alex Walpole <alexcwalpole@gmail.com>
2025-12-28 19:28:39 -0500

    feat(bat): new themes and use specific sha insteas of branch names

--- home/bat.nix
+++ home/bat.nix
@@ -22,7 +22,7 @@
           src = pkgs.fetchFromGitHub {
             owner = "folke";
             repo = "tokyonight.nvim";
-            rev = "main";
+            rev = "5da1b76e64daf4c5d410f06bcb6b9cb640da7dfd";
             sha256 = "sha256-4zfkv3egdWJ/GCWUehV0MAIXxsrGT82Wd1Qqj1SCGOk=";
           };
           file = "extras/sublime/tokyonight_night.tmTheme";
@@ -31,7 +31,7 @@
           src = pkgs.fetchFromGitHub {
             owner = "rebelot";
             repo = "kanagawa.nvim";
-            rev = "master";
+            rev = "aef7f5cec0a40dbe7f3304214850c472e2264b10";
             sha256 = "sha256-nHcQWTX4x4ala6+fvh4EWRVcZMNk5jZiZAwWhw03ExE=";
           };
           file = "extras/tmTheme/kanagawa.tmTheme";
@@ -40,14 +40,30 @@
           src = pkgs.fetchFromGitHub {
             owner = "rose-pine";
             repo = "tm-theme";
-            rev = "master";
-            # sha256 = "sha256-nHcQWTX4x4ala6+fvh4EWRVcZMNk5jZiZAwWhw03ExE=";
+            rev = "417d201beb5f0964faded5448147c252ff12c4ae";
             sha256 = "sha256-aNDOqY81FLFQ6bvsTiYgPyS5lJrqZnFMpvpTCSNyY0Y=";
           };
           file = "dist/rose-pine.tmTheme";
         };
+        catppuccin = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "6810349b28055dce54076712fc05fc68da4b8ec0";
+            sha256 = "sha256-lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
+          };
+          file = "themes/catppuccin.tmTheme";
+        };
+        nightfox = {
+          src = pkgs.fetchFromGitHub {
+            owner = "EdenEast";
+            repo = "nightfox.nvim";
+            rev = "ba47d4b4c5ec308718641ba7402c143836f35aa9";
+            sha256 = "sha256-HoZEwncrUnypWxyB+XR0UQDv+tNu1/NbvimxYGb7qu8";
+          };
+          file = "extra/nightfox/nightfox.tmTheme";
+        };
       };
-
       config = {
         theme = config.bat.theme;
       };
