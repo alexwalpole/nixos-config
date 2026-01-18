6ab20ddccfdb16417b68facc1fdd224233b15715
Alex Walpole <alexcwalpole@gmail.com>
2026-01-18 17:24:12 -0500

    chore(darwin): uses ghostty bin package
    this means less hacks for things like bat. stops the program from being
    available in spotlight though :/

--- darwin/home.nix
+++ darwin/home.nix
@@ -4,18 +4,18 @@
     ../home.nix
   ];
   # ghostty isn't available in nixpkgs for darwin yet :/
-  programs.ghostty.package = pkgs.emptyDirectory;
-  programs = {
-    ghostty.installBatSyntax = false;
-    bat = {
-      config.map-syntax = [ "${config.xdg.configHome}/ghostty/config:Ghostty Config" ];
-
-      syntaxes.ghostty = {
-        src = "/";
-        file = "Applications/Ghostty.app/Contents/Resources/bat/syntaxes/ghostty.sublime-syntax";
-      };
-    };
-  };
+  programs.ghostty.package = pkgs.ghostty-bin;
+  # programs = {
+  #   ghostty.installBatSyntax = false;
+  #   bat = {
+  #     config.map-syntax = [ "${config.xdg.configHome}/ghostty/config:Ghostty Config" ];
+  #
+  #     syntaxes.ghostty = {
+  #       src = "/";
+  #       file = "Applications/Ghostty.app/Contents/Resources/bat/syntaxes/ghostty.sublime-syntax";
+  #     };
+  #   };
+  # };
   home.username = lib.mkForce "alexwalpole";
   home.homeDirectory = lib.mkForce "/Users/alexwalpole";
 
