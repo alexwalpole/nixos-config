922e8c52ab091da2110f9aab6314e2309c2844a4
Alex Walpole <alexcwalpole@gmail.com>
2026-01-18 11:20:38 -0500

    fix: bat doesn't break nvim on darwin
    this is ugly as hell but there's some weirdness because i am using an
    emptyDirectory as the ghostty package. it results in bat not being able
    to find ghostty syntaxes, and that maybe impacting the home manager buid
    (???) and then nvim not working as a result.

--- darwin/home.nix
+++ darwin/home.nix
@@ -1,11 +1,21 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 {
   imports = [
     ../home.nix
   ];
   # ghostty isn't available in nixpkgs for darwin yet :/
   programs.ghostty.package = pkgs.emptyDirectory;
+  programs = {
+    ghostty.installBatSyntax = false;
+    bat = {
+      config.map-syntax = [ "${config.xdg.configHome}/ghostty/config:Ghostty Config" ];
 
+      syntaxes.ghostty = {
+        src = "/";
+        file = "Applications/Ghostty.app/Contents/Resources/bat/syntaxes/ghostty.sublime-syntax";
+      };
+    };
+  };
   home.username = lib.mkForce "alexwalpole";
   home.homeDirectory = lib.mkForce "/Users/alexwalpole";
 
