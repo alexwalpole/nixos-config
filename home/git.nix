76803c6715e8731d9e0a9bbd67a0776089003d08
Alex Walpole <alexcwalpole@gmail.com>
2026-01-19 19:10:29 -0500

    feat(git): uses nvim for editor..?

--- home/git.nix
+++ home/git.nix
@@ -2,9 +2,12 @@
 {
   programs.git = {
     enable = true;
-    settings.user = {
-      name = "Alex Walpole";
-      email = "alexcwalpole@gmail.com";
+    settings = {
+      user = {
+        name = "Alex Walpole";
+        email = "alexcwalpole@gmail.com";
+      };
+      core.editor = "nvim";
     };
     ignores = [ ".DS_Store" ];
   };
