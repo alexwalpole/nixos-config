32426b185b702bc6b01f3201dbdaeff73bcd443f
Alex Walpole <alexcwalpole@gmail.com>
2026-02-07 23:05:33 -0500

    feat(nvim): undotree toggle tweak

--- home/nixvim/plugins/undotree.nix
+++ home/nixvim/plugins/undotree.nix
@@ -6,7 +6,10 @@
         mode = "n";
         key = "<leader>u";
         #TODO: figure out how to use mkRaw instead
-        action.__raw = "vim.cmd.UndotreeToggle";
+        action.__raw = ''function()
+		vim.cmd.UndotreeToggle()
+		vim.cmd.UndotreeFocus()
+	end'';
       }
     ];
 
